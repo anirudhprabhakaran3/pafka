a805a5342420827e8c9df98a936e2050ae0cd280
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2025-10-30 22:03:58 +0100

    Added node self-register to broker

--- node/main.py
+++ node/main.py
@@ -4,6 +4,7 @@ from models.metadata import NodeMetadata
 from models.api import RegisterTopicRequest
 import logging
 from storage import InMemoryStorage
+import requests
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -12,9 +13,26 @@ app = FastAPI()
 metadata = NodeMetadata(
     identifier=os.environ.get("PAFKA_NODE_IDENTIFIER"),
     broker=os.environ.get("PAFKA_BROKER"),
+    host=os.environ.get("PAFKA_NODE_HOST"),
     port=int(os.environ.get("PAFKA_NODE_PORT")),
 )
 storage = InMemoryStorage()
+
+# Register to Broker
+broker_url = os.environ.get("PAFKA_BROKER")
+broker_register_endpoint = f"http://{broker_url}/connections/register"
+register_request_payload = {
+    "node_identifier": metadata.identifier,
+    "node_ip": metadata.host + ":" + str(metadata.port)
+}
+register_response = requests.post(broker_register_endpoint, json=register_request_payload)
+logger.info(f"Sending register request to {broker_register_endpoint} with payload: {register_request_payload}")
+if (register_response.status_code == 200):
+    resp = register_response.json()
+    logger.info(f"Request returned a response {resp}") 
+else:
+    logger.error(f"Unable to register with broker at {broker_register_endpoint} with payload {register_request_payload}")
+    exit()
 
 logger.info(f"Initialized server with NodeMetadata: {metadata}")
 
--- node/models/metadata.py
+++ node/models/metadata.py
@@ -3,4 +3,5 @@ from pydantic import BaseModel
 class NodeMetadata(BaseModel):
     identifier: str
     broker: str
+    host: str
     port: int
